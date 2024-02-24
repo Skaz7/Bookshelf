760378ff588cd36a456bf735eef301f5e5abcbf5
Sebastian Kupis <sebastian.kupis@gmail.com>
2024-02-24 21:57:55 +0100

    work on menu

--- main.py
+++ main.py
@@ -5,11 +5,7 @@ from Books import PrintedBook
 
 
 database_file = "./Data/bookshelf.db"
-
-book1 = PrintedBook("The Hobbit", "J.R.R. Tolkien", "Fantasy", 1937, "23.02.2024", 153)
-book2 = PrintedBook(
-    "The Lord of the Rings", "J.R.R. Tolkien", "Fantasy", 1954, "24.02.2024", 1178
-)
+menu = MainMenu()
 
 
 def main():
@@ -19,14 +15,7 @@ def main():
 
 if __name__ == "__main__":
     if database_exist(database_file):
-        conn = create_connection(database_file)
-        create_table(conn, "PrintedBooks")
-        # delete_table(conn, "PrintedBooks")
-        add_book_to_database(conn, book1, "PrintedBooks")
-        add_book_to_database(conn, book2, "PrintedBooks")
-        input()
-        remove_book_from_database(conn, book1, "PrintedBooks")
-        conn.close()
+        menu.run()
 
     else:
         print("\nCan't find database file!\n")
--- menu.py
+++ menu.py
@@ -1,23 +1,23 @@
 import os
-from catalog import Catalog
-from dbfunctions import show_books
+from dbfunctions import *
 import time
 from datetime import datetime
-from Books import Book
+from Books import PrintedBook
 
-# from Functionality import add_book
+
+database_file = "./Data/bookshelf.db"
 
 
 class MainMenu:
     """Displays menu and responds to choices when run."""
 
     def __init__(self):
-        self.catalog = Catalog()
         self.choices = {
             "1": self.show_bookshelf,
             "2": self.search_books,
-            "3": self.new_book,
+            "3": self.new_printed_book,
             "4": self.edit_book,
+            "5": self.delete_book,
             "0": self.quit,
         }
 
@@ -30,6 +30,7 @@ class MainMenu:
     2. Search for book
     3. Add new book
     4. Edit book info
+    5. Delete book
 
     0. QUIT
 """
@@ -60,7 +61,7 @@ class MainMenu:
         time.sleep(2)
         return
 
-    def new_book(self):
+    def new_printed_book(self):
         """Create a new book with the given information."""
         print("Adding new book to library:")
 
@@ -69,28 +70,33 @@ class MainMenu:
         genre = input("Book Genre: ").title()
         year = input("Book Year: ")
         add_date = datetime.today().date()
+        pages = input("Number of Pages: ")
 
-        new_book = Book(title, author, genre, year, add_date)
+        new_book = PrintedBook(title, author, genre, year, add_date, pages)
 
         while True:
             choice = input("\nAdd this book to the Library? (Y/N)  > \n").lower()
             if choice == "y" or choice == "yes":
-                self.catalog.add_book(new_book)
+                conn = create_connection(database_file)
+                add_book_to_database(conn, new_book, "PrintedBooks")
+                conn.close()
                 print("Book added to library.")
-                print(self.catalog.books)
                 time.sleep(2)
                 return
-            
+
             elif choice == "n" or choice == "no":
                 print("Book not added.")
                 time.sleep(2)
                 return
-            
+
             else:
                 print("Invalid input.")
                 time.sleep(2)
 
     def edit_book(self):
+        pass
+
+    def delete_book(self):
         pass
 
     def quit(self):
